5e233c9e2cd96d470799dcdb88a39ce353b2296b
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-10-06 14:45:14 +0300

    close

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,14 +37,14 @@ export default function Navbar() {
                 <button onClick={handleopen} className='md:hidden'><i className="bi bi-list text-3xl"></i></button>
             </div>
         </nav>
-        {isOpen && <div className={`${isOpen?'opacity-100':'opacity-0'} absolute inset-0 bg-black/45 transition-all duration-300`}  />}
+        {isOpen && <div onClick={()=>setIsOpen(false)} className={`${isOpen?'opacity-100':'opacity-0'} absolute inset-0 bg-black/45 transition-all duration-300`}  />}
         <div className={`${isOpen ? 'translate-x-0' : 'translate-x-[150%]'} transition-all duration-300 fixed top-0 bottom-0 end-0 w-[300px] bg-white dark:bg-bg text-text-one`}>
             <button onClick={() => setIsOpen(false)} className='absolute end-4 top-4 text-[22px]'>
                 <i className='bi bi-x-lg'></i>
             </button>
             <ul className={` flex flex-col space-y-3 absolute left-4 top-9`}>
                 {links.map((link, i) => (
-                    <NavLink to={`${link}`} className={`${link === 'faq' ? 'uppercase' : 'capitalize'}`}>{link}</NavLink>
+                    <NavLink onClick={()=>setIsOpen(false)} to={`${link}`} className={`${link === 'faq' ? 'uppercase' : 'capitalize'}`}>{link}</NavLink>
                 ))}
             </ul>
             <button className='absolute left-4 bottom-4 flex items-center  gap-x-2 rounded-full bg-purple text-white px-4 py-2  btn font-Fredoka font-bold'><i className="bi bi-heart text-[16px] mt-1"></i> <span>Donate Now</span></button>
