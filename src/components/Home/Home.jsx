b1a62da503ae09888d33aa37409dcbc5e9646f24
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-10-07 13:53:48 +0300

    daf

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
 
     return (
-        <div className='p min-h-screen flex md:flex-row flex-col  items-center justify-between bg-bg transition-colors duration-300'>
+        <div className='p min-h-screen flex md:flex-row flex-col gap-y-4 md:gap-y-0 items-center justify-between bg-bg transition-colors duration-300'>
             <div className='md:w-5/12 md:-translate-y-5 flex flex-col gap-y-6 md:gap-y-0 transition-colors duration-300 text-center md:text-left  '>
                 <p className='font-Quicksand text-purple w-fit text-[24px] flex items-center md:mb-[24px] font-semibold bg-purple-400/15 p-4 rounded-full mx-auto md:mx-0'> <i className="bi bi-globe-americas text-sm me-1 text-text-one "></i>Trusted by 50,000+ pet lovers</p>
                 <h2 className='md:text-[56px] text-4xl font-Fredoka font-bold md:leading-16 text-text-one md:mb-[24px]'><span className='block' >Adopt love,</span>foster happiness.</h2>
@@ -21,7 +21,7 @@ export default function Home() {
                     <button className='shadow-xl cursor-pointer flex items-center text-[16px] gap-x-2 rounded-full bg-purple text-white px-[32px] py-[16px]  btnn font-Fredoka font-semibold'><span>Adopt Now</span></button>
                     <button className='shadow-xl cursor-pointer flex items-center text-[16px] gap-x-2 rounded-full bg-white dark:bg-[#313130] text-purple border-2 border-purple px-[32px] py-[16px]  btnn font-Fredoka font-semibold'><span>Donate Now</span></button>
                 </div>
-                <div className='flex items-center space-x-3 mt-7 text-center md:text-left'>
+                <div className='flex items-center space-x-3 mt-7 text-center mx-auto md:mx-0'>
                     <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>50K+</span><span className='font-Quicksand text-base text-text-two'>Happy Adopters</span></div>
                     <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>15K+</span><span className='font-Quicksand text-base text-text-two'>Pets Adopted</span></div>
                     <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>200+</span><span className='font-Quicksand text-base text-text-two'>Partner Shelters</span></div>
