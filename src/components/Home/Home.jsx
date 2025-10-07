a3579e6f8cc07f4c04740071ecc13c8f40077e11
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-10-07 13:50:44 +0300

    fda

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 
+import dog1 from '../../assets/dog1.jpg'
+import dog2 from '../../assets/dog2.jpg'
+import cat1 from '../../assets/cat1.jpg'
+import cat2 from '../../assets/cat2.jpg'
+
 export default function Home() {
 
 
@@ -7,8 +12,31 @@ export default function Home() {
 
 
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='p min-h-screen flex md:flex-row flex-col  items-center justify-between bg-bg transition-colors duration-300'>
+            <div className='md:w-5/12 md:-translate-y-5 flex flex-col gap-y-6 md:gap-y-0 transition-colors duration-300 text-center md:text-left  '>
+                <p className='font-Quicksand text-purple w-fit text-[24px] flex items-center md:mb-[24px] font-semibold bg-purple-400/15 p-4 rounded-full mx-auto md:mx-0'> <i className="bi bi-globe-americas text-sm me-1 text-text-one "></i>Trusted by 50,000+ pet lovers</p>
+                <h2 className='md:text-[56px] text-4xl font-Fredoka font-bold md:leading-16 text-text-one md:mb-[24px]'><span className='block' >Adopt love,</span>foster happiness.</h2>
+                <p className='font-Quicksand text-[18px] text-text-two'>Find your forever friend fast, safe, and loving. Connect with verified shelters and adopt with confidence.</p>
+                <div className='flex items-center md:mt-[24px] space-x-5 mx-auto md:mx-0'>
+                    <button className='shadow-xl cursor-pointer flex items-center text-[16px] gap-x-2 rounded-full bg-purple text-white px-[32px] py-[16px]  btnn font-Fredoka font-semibold'><span>Adopt Now</span></button>
+                    <button className='shadow-xl cursor-pointer flex items-center text-[16px] gap-x-2 rounded-full bg-white dark:bg-[#313130] text-purple border-2 border-purple px-[32px] py-[16px]  btnn font-Fredoka font-semibold'><span>Donate Now</span></button>
+                </div>
+                <div className='flex items-center space-x-3 mt-7 text-center md:text-left'>
+                    <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>50K+</span><span className='font-Quicksand text-base text-text-two'>Happy Adopters</span></div>
+                    <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>15K+</span><span className='font-Quicksand text-base text-text-two'>Pets Adopted</span></div>
+                    <div className='flex flex-col '><span className='font-Fredoka font-bold text-purple md:text-[30px]'>200+</span><span className='font-Quicksand text-base text-text-two'>Partner Shelters</span></div>
+                </div>
+            </div>
+            <div className='md:w-6/12'>
+
+                <div className=' grid md:grid-cols-2 gap-3'>
+                    <div className='card'><img className='w-full h-full object-cover' src={dog1} alt="" /></div>
+                    <div className='card'><img className='w-full h-full object-cover' src={cat1} alt="" /></div>
+                    <div className='card'><img className='w-full h-full object-cover' src={cat2} alt="" /></div>
+                    <div className='card'><img className='w-full h-full object-cover' src={dog2} alt="" /></div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import logo from "../../assets/logo.png"
 import logoDark from "../../assets/logodark.png"
@@ -15,13 +15,13 @@ export default function Navbar() {
         document.documentElement.classList.toggle('dark')
         setdark(prev => !prev)
     }
-
+ 
     const handleopen = () => {
         setIsOpen(prev => !prev)
     }
 
     return (<>
-        <nav className={`bg-white dark:bg-[#313130] padding shadow-md flex justify-between items-center `}>
+        <nav className={`bg-white dark:bg-[#313130] transition-colors duration-300 padding shadow-md flex justify-between items-center fixed top-0 w-full `}>
             <Link to={'/'} className='md:w-40 w-28 block'>
                 <img src={logo} className='dark:hidden' alt="" />
                 <img src={logoDark} className='dark:block hidden' alt="" />
@@ -37,14 +37,14 @@ export default function Navbar() {
                 <button onClick={handleopen} className='md:hidden'><i className="bi bi-list text-3xl"></i></button>
             </div>
         </nav>
-        {isOpen && <div onClick={()=>setIsOpen(false)} className={`${isOpen?'opacity-100':'opacity-0'} absolute inset-0 bg-black/45 transition-all duration-300`}  />}
+        {isOpen && <div onClick={() => setIsOpen(false)} className={`${isOpen ? 'opacity-100' : 'opacity-0'} fixed inset-0  bg-black/45 transition-all duration-300`} />}
         <div className={`${isOpen ? 'translate-x-0' : 'translate-x-[150%]'} transition-all duration-300 fixed top-0 bottom-0 end-0 w-[300px] bg-white dark:bg-bg text-text-one`}>
             <button onClick={() => setIsOpen(false)} className='absolute end-4 top-4 text-[22px]'>
                 <i className='bi bi-x-lg'></i>
             </button>
             <ul className={` flex flex-col space-y-3 absolute left-4 top-9`}>
                 {links.map((link, i) => (
-                    <NavLink onClick={()=>setIsOpen(false)} to={`${link}`} className={`${link === 'faq' ? 'uppercase' : 'capitalize'}`}>{link}</NavLink>
+                    <NavLink onClick={() => setIsOpen(false)} to={`${link}`} className={`${link === 'faq' ? 'uppercase' : 'capitalize'}`}>{link}</NavLink>
                 ))}
             </ul>
             <button className='absolute left-4 bottom-4 flex items-center  gap-x-2 rounded-full bg-purple text-white px-4 py-2  btn font-Fredoka font-bold'><i className="bi bi-heart text-[16px] mt-1"></i> <span>Donate Now</span></button>
